e34ee2e11c76939d521d7918af840d47a0b776c7
Om Aryan <aryan2003k@gmail.com>
2025-05-11 11:43:30 +0530

    backend setup

---
+++ mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/MiniSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.cyberpsych.mini_search_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSearchEngineApplication.class, args);
+	}
+
+}
---
+++ mini-search-engine/src/test/java/com/cyberpsych/mini_search_engine/MiniSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberpsych.mini_search_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
