5701c3a59bd41c191e1372faa46ef596b77e076c
Om Aryan <aryan2003k@gmail.com>
2025-05-13 17:04:24 +0530

    queue-based BFS crawling implemented

--- mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/services/WebCrawlerService.java
+++ mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/services/WebCrawlerService.java
@@ -1,4 +1,8 @@
 package com.cyberpsych.mini_search_engine.services;
+import com.cyberpsych.mini_search_engine.entities.Link;
+import com.cyberpsych.mini_search_engine.entities.Page;
+import com.cyberpsych.mini_search_engine.repositories.LinkRepository;
+import com.cyberpsych.mini_search_engine.repositories.PageRepository;
 import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 import org.jsoup.Jsoup;
@@ -15,7 +21,15 @@ import org.jsoup.nodes.Document;
 public class WebCrawlerService {
     @Value("${crawler.user-agent}")
     private String userAgent;
+    private final PageRepository pageRepository;
+    private final LinkRepository linkRepository;
+
     private static final Logger logger = LoggerFactory.getLogger(WebCrawlerService.class);
+
+    public WebCrawlerService(PageRepository pageRepository, LinkRepository linkRepository){
+        this.pageRepository = pageRepository;
+        this.linkRepository = linkRepository;
+    }
 
     public Document fetchPage(String url) throws IOException{
         logger.info("Fetching URL: {}",url);
@@ -30,9 +44,51 @@ public class WebCrawlerService {
 
         for(Element link: doc.select("a[href]")){
             String href = link.attr("abs:href");
-            if (!href.isEmpty())
+            if (!href.isEmpty() && href.startsWith("http"))
                 links.add(href);
         }
+        logger.info("Found {} links on {}", links.size(), url);
         return links;
+    }
+    public void crawl(String seedUrl){
+        Queue<Page> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        Page seedPage = new Page();
+        seedPage.setUrl(seedUrl);
+        pageRepository.save(seedPage);
+
+        queue.add(seedPage);
+        visited.add(seedUrl);
+
+        while(!queue.isEmpty()){
+            Page currentPage = queue.poll();
+
+            try{
+                Set<String> links = extractLinks(currentPage.getUrl());
+                for (String linkUrl : links){
+                    if (!visited.contains(linkUrl)){
+                        
+                        //save linked page
+                        Page linkedPage = new Page();
+                        linkedPage.setUrl(linkUrl);
+                        linkedPage = pageRepository.save(linkedPage);
+
+                        //save link relationship
+                        Link link = new Link();
+                        link.setFrom(currentPage);
+                        link.setTo(linkedPage);
+                        linkRepository.save(link);
+
+                        // track visited pages
+                        queue.add(linkedPage);
+                        visited.add(linkUrl);
+                    }
+                }
+            }
+            catch (IOException e){
+                logger.error("Error crawling {}",currentPage.getUrl(), e.getMessage());
+            }
+        }
     }
 }
