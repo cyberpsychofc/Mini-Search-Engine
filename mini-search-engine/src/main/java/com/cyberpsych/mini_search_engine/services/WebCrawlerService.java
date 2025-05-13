c39ec4b7efa08eeb7b025c58ca3984471e158a9e
Om Aryan <aryan2003k@gmail.com>
2025-05-13 09:30:56 +0530

    added slf4j logging to methods

--- mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/services/WebCrawlerService.java
+++ mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/services/WebCrawlerService.java
@@ -1,7 +1,13 @@
 package com.cyberpsych.mini_search_engine.services;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -9,8 +15,24 @@ import org.jsoup.nodes.Document;
 public class WebCrawlerService {
     @Value("${crawler.user-agent}")
     private String userAgent;
+    private static final Logger logger = LoggerFactory.getLogger(WebCrawlerService.class);
 
     public Document fetchPage(String url) throws IOException{
+        logger.info("Fetching URL: {}",url);
+        Document doc = Jsoup.connect(url).userAgent(userAgent).get();
+        logger.debug("Fetched document from {} with title: {}",url, doc.title());
         return Jsoup.connect(url).get();
+    }
+    public Set<String> extractLinks(String url) throws IOException{
+        logger.info("Extracting links from {}",url);
+        Set<String> links = new HashSet<>();
+        Document doc = fetchPage(url);
+
+        for(Element link: doc.select("a[href]")){
+            String href = link.attr("abs:href");
+            if (!href.isEmpty())
+                links.add(href);
+        }
+        return links;
     }
 }
