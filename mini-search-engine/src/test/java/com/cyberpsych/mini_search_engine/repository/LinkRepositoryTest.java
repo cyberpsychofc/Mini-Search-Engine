040eb5822c74044820ec4277e32a105d0e1eae44
Om Aryan <aryan2003k@gmail.com>
2025-05-15 16:10:49 +0530

    added junit tests

---
+++ mini-search-engine/src/test/java/com/cyberpsych/mini_search_engine/repository/LinkRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.cyberpsych.mini_search_engine.repository;
+
+import com.cyberpsych.mini_search_engine.entities.Link;
+import com.cyberpsych.mini_search_engine.entities.Page;
+import com.cyberpsych.mini_search_engine.repositories.LinkRepository;
+import com.cyberpsych.mini_search_engine.repositories.PageRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class LinkRepositoryTest {
+
+    @Autowired
+    private PageRepository pageRepository;
+    @Autowired
+    private LinkRepository linkRepository;
+
+    @Test
+    public void shouldSaveAndFindLink(){
+        Page fromPage = new Page();
+        fromPage.setUrl("https://example.com");
+        Page toPage = new Page();
+        toPage.setUrl("https://example.org");
+        pageRepository.save(fromPage);
+        pageRepository.save(toPage);
+
+        //create link
+        Link link = new Link();
+        link.setFrom(fromPage);
+        link.setTo(toPage);
+
+        //save to db
+        Link savedLink = linkRepository.save(link);
+
+        //verify it was saved
+        assertThat(savedLink.getId()).isNotNull();
+        assertThat(savedLink.getFrom().getUrl()).isEqualTo("https://example.com");
+        assertThat(savedLink.getTo().getUrl()).isEqualTo("https://example.org");
+
+        //find by ID
+        Link foundLink = new Link();
+        assertThat(foundLink).isNotNull();
+        assertThat(foundLink.getFrom().getUrl()).isEqualTo("https://example.com");
+    }
+}
---
+++ mini-search-engine/src/test/java/com/cyberpsych/mini_search_engine/service/WebcrawlerServiceTest.java
@@ -0,0 +1,73 @@
+package com.cyberpsych.mini_search_engine.service;
+
+import com.cyberpsych.mini_search_engine.repositories.LinkRepository;
+import com.cyberpsych.mini_search_engine.repositories.PageRepository;
+import com.cyberpsych.mini_search_engine.services.WebCrawlerService;
+import com.google.common.util.concurrent.RateLimiter;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Set;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class WebcrawlerServiceTest {
+
+    @Autowired
+    private WebCrawlerService webCrawlerService;
+    @MockitoBean
+    private PageRepository pageRepository;
+    @MockitoBean
+    private LinkRepository linkRepository;
+    @MockitoBean
+    private RateLimiter rateLimiter;
+
+    private MockWebServer mockWebServer;
+    private static final Logger logger = LoggerFactory.getLogger(WebCrawlerService.class);
+
+
+    @BeforeEach
+    void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+    @AfterEach
+    void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    public void shouldExtractLinksFromPage() throws IOException {
+        String html = """
+            <html>
+            <body>
+                <a href="/page1">Page 1</a>
+                <a href="https://example.org">External Page</a>
+            </body>
+            </html>
+            """;
+        mockWebServer.enqueue(new MockResponse().setBody(html).addHeader("Content-Type","text/html"));
+
+        // extract links
+        String testUrl = mockWebServer.url("/").toString();
+        Set<String> links = webCrawlerService.extractLinks(testUrl);
+
+        //verify
+        assertThat(links).hasSize(2);
+        assertThat(links).contains(
+                testUrl + "page1",
+                "https://example.org"
+        );
+    }
+}
