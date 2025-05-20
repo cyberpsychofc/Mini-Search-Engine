10a4c8885b5392d53f1913f92641797e7a36bf5c
Om Aryan <aryan2003k@gmail.com>
2025-05-20 11:58:08 +0530

    started building Search API

--- mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/repositories/PostingRepository.java
+++ mini-search-engine/src/main/java/com/cyberpsych/mini_search_engine/repositories/PostingRepository.java
@@ -3,5 +3,8 @@ package com.cyberpsych.mini_search_engine.repositories;
 import com.cyberpsych.mini_search_engine.entities.PostingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostingRepository extends JpaRepository<PostingEntity, Long> {
+    List<PostingEntity> findByTerm(String term);
 }
